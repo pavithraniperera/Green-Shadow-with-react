f10ba7e27442fb93f97c11c126884c52854007dc
pavithrani <pererapavithrani02@gmail.com>
2025-01-16 20:19:51 +0530

    update field page with components

--- src/Pages/FieldsPage.tsx
+++ src/Pages/FieldsPage.tsx
@@ -1,5 +1,31 @@
+import SearchBarComponent from "../Components/SearchBarComponent";
+import FieldContainer from "../Components/FieldContainer.tsx";
+import AddBtnComponent from "../Components/AddBtnComponent.tsx";
+import  {useState} from "react";
+import AddFieldModal from "../Components/FieldModalComponent/AddFieldModal.tsx";
+
 export default function FieldPage() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
     return (
-        <h1>Field</h1>
+        <>
+            <SearchBarComponent placeholder="Search Field by Location.." onChange="" />
+            <section className="flex justify-center items-center pt-10 md:pl-20 mx-auto">
+                <FieldContainer/>
+            </section>
+            <div className="flex justify-center items-center pt-10 md:pl-20 mx-auto font-itim ">
+                <AddBtnComponent text="Add Field" onClick={openModal}/>
+            </div>
+            {isModalOpen && <AddFieldModal isOpen={isModalOpen} onClose={closeModal} />}
+
+
+
+
+
+        </>
+
+
     )
 }
