c09dbaaee0b64fc7fcaf4a73938d054492369a0c
pavithrani <pererapavithrani02@gmail.com>
2025-01-16 20:16:55 +0530

    create AddBtnComponent

---
+++ src/Components/AddBtnComponent.tsx
@@ -0,0 +1,11 @@
+export default function AddBtnComponent(props) {
+    return (
+        <button
+            onClick={props.onClick}
+            className="w-60 h-12 font-bold bg-[rgba(242,215,182,0.63)] text-[#274D40] text-lg rounded-lg cursor-pointer transition-all ease-in-out duration-300 shadow-md hover:bg-[#8ad38d] hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#274D40] flex items-center justify-center text-xl"
+        >
+            <i className="fas fa-plus-circle mr-2"></i>
+            {props.text}
+        </button>
+    )
+}
