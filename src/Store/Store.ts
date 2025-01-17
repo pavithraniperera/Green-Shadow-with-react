7778dc2930bf97e5b225b88092135d3479a8748e
pavithrani <pererapavithrani02@gmail.com>
2025-01-17 19:56:32 +0530

    update store adding field slice

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 
 import authReducer from "../Features/AuthSlice.ts";
+import fieldReducer from "../Features/FieldSlice.ts";
 
 const store = configureStore({
     reducer: {
         auth:authReducer,
+        field:fieldReducer,
     }
 });
 export default store;
