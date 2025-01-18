4f1fc5dbd334cd9e2c7a61eb92533b2d63aff257
pavithrani <pererapavithrani02@gmail.com>
2025-01-18 20:17:53 +0530

    add crop reduce to store

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 
 import authReducer from "../Features/AuthSlice.ts";
 import fieldReducer from "../Features/FieldSlice.ts";
+import cropReducer from "../Features/CropSlice.ts";
 
 const store = configureStore({
     reducer: {
         auth:authReducer,
         field:fieldReducer,
+        crop:cropReducer
     }
 });
 export default store;
