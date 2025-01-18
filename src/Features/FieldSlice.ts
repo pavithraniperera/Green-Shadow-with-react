57326ec261f0c5c6862382dcdcfbe8971896d68d
pavithrani <pererapavithrani02@gmail.com>
2025-01-18 11:10:30 +0530

    add update functions,delete functions

--- src/Features/FieldSlice.ts
+++ src/Features/FieldSlice.ts
@@ -15,8 +15,20 @@ const FieldSlice = createSlice({
             console.log('Updated Fields ArrayS:', state.fields);
 
 
+        },
+        updateField:(state,action)=>{
+            console.log('Action Payload:', action.payload);
+            const {fieldId,updatedField}=action.payload;
+            state.fields = state.fields.map(field =>
+                field.fieldId === fieldId ? { ...field, ...updatedField } : field
+            );
+        },
+        deleteField:(state,action)=>{
+
+           state.fields = state.fields.filter(field =>field.fieldId !== action.payload);
         }
+
     }
 })
-export const {addField} = FieldSlice.actions;
+export const {addField,updateField,deleteField} = FieldSlice.actions;
 export default FieldSlice.reducer;
