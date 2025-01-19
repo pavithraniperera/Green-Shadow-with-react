d2005bb88be0d7447529088aa1a56bbcff1ac53d
pavithrani <pererapavithrani02@gmail.com>
2025-01-19 18:11:19 +0530

    add staff reducer to store

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -3,12 +3,14 @@ import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "../Features/AuthSlice.ts";
 import fieldReducer from "../Features/FieldSlice.ts";
 import cropReducer from "../Features/CropSlice.ts";
+import staffReducer from "../Features/StaffSlice.ts";
 
 const store = configureStore({
     reducer: {
         auth:authReducer,
         field:fieldReducer,
-        crop:cropReducer
+        crop:cropReducer,
+        staff:staffReducer
     }
 });
 export default store;
