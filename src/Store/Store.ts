b4c0c5743799e2532bb3b618d95157713d459495
pavithrani <pererapavithrani02@gmail.com>
2025-01-20 12:03:40 +0530

    add equipment slice and vehicle slice

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -4,13 +4,17 @@ import authReducer from "../Features/AuthSlice.ts";
 import fieldReducer from "../Features/FieldSlice.ts";
 import cropReducer from "../Features/CropSlice.ts";
 import staffReducer from "../Features/StaffSlice.ts";
+import vehicleReducer from "../Features/VehicleSlice.ts";
+import equipmentReducer from "../Features/EquipmentSlice.ts";
 
 const store = configureStore({
     reducer: {
         auth:authReducer,
         field:fieldReducer,
         crop:cropReducer,
-        staff:staffReducer
+        staff:staffReducer,
+        vehicle:vehicleReducer,
+        equipment:equipmentReducer
     }
 });
 export default store;
