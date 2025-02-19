ba3506a0697492f1fac40a4027329bd66dbf024d
pavithrani <pererapavithrani02@gmail.com>
2025-02-19 19:56:31 +0530

    Did some changes in Auth modal

--- src/Features/AuthSlice.ts
+++ src/Features/AuthSlice.ts
@@ -1,7 +1,6 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import axios from "axios";
-import {fetchFields} from "./FieldSlice.ts";
-import {fetchCrops} from "./CropSlice.ts";
+
 
 
 
